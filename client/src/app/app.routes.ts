76423a2cfbb70e52d6e91e890510d79b083ff844
ZlatkoMarianov <zlatkomarianov@abv.bg>
2026-03-25 18:15:00 +0200

    Add login/register/404 pages and update routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { CatalogComponent } from './features/catalog/catalog.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+
+    { path: 'home', component: HomeComponent },
+
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
     { path: 'catalog', component: CatalogComponent },
+
+    { path: '**', component: NotFoundComponent },
 ];
