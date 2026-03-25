8075f271820176a336e8b520f6520c4b1f84e9cd
ZlatkoMarianov <zlatkomarianov@abv.bg>
2026-03-25 19:08:46 +0200

    Add product features and product-card component

---
+++ client/src/app/features/my-products/my-products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-my-products',
+  imports: [RouterLink],
+  templateUrl: './my-products.component.html',
+  styleUrl: './my-products.component.css',
+})
+export class MyProductsComponent { }
