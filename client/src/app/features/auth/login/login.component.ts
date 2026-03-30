29f3634ee4b61cad4b78c5f77af8c01ef55909d0
ZlatkoMarianov <zlatkomarianov@abv.bg>
2026-03-30 20:54:03 +0300

    Implement login form

--- client/src/app/features/auth/login/login.component.ts
+++ client/src/app/features/auth/login/login.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { emailValidator } from '../../../shared/validators/email.validator';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  isLoading = false;
+  errorMessage = '';
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, emailValidator()]],
+    password: ['', [Validators.required]],
+  });
+
+  onLogin(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (user) => {
+        this.authService.setUser(user);
+        this.isLoading = false;
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.errorMessage = err.error?.message || 'Login failed. Please try again.';
+      },
+    });
+  }
+}
